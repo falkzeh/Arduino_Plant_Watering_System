7cdaeccc61143378a1a400e48a04b0bbacf59e5e
falkzeh <mail@falkzeh.io>
2023-05-17 15:34:19 +0200

    initial

---
+++ src/plant_watering.ino
@@ -0,0 +1,32 @@
+const int NUM_SENSORS = 4;
+int outputPins[NUM_SENSORS] = {2, 3, 4, 5};
+int inputPins[NUM_SENSORS] = {A0, A1, A2, A3};
+float values[NUM_SENSORS] = {0, 0, 0, 0};
+
+void setup() {
+  Serial.begin(9600);
+
+  for(int i = 0; i < NUM_SENSORS; i++) {
+    pinMode(outputPins[i], OUTPUT);
+    pinMode(inputPins[i], INPUT);
+    digitalWrite(outputPins[i], HIGH);
+  }
+  delay(500);
+}
+
+void loop() {
+  for(int i = 0; i < NUM_SENSORS; i++) {
+    Serial.print("MOISTURE LEVEL:");
+    values[i] = analogRead(inputPins[i]);
+    Serial.println(values[i]);
+
+    if(values[i] > 750) {
+      digitalWrite(outputPins[i], LOW);
+    } else {
+      digitalWrite(outputPins[i], HIGH);
+    }
+  }
+
+  Serial.println();
+  delay(1000);
+}
